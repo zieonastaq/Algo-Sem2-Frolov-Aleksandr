91000c98ebbb2d0d85cf6259e0a4da3ee9a178df
zieonasta <zieonasta@ya.ru>
2021-03-03 13:42:05 +0300

    Hash problem A some changes

--- HashProblemA/main.cpp
+++ HashProblemA/main.cpp
@@ -3,11 +3,30 @@
 #include <vector>
 
 class HashTable {
+ public:
+  HashTable() : table_of_values_(M), table_of_strings_(M) {}
+
+  long long AddAndReturnValue(const std::string& str, long long value) {
+    long long hash = Hash(str);
+
+    for (int i = 0; i < table_of_strings_[hash].size(); ++i) {
+      if (table_of_strings_[hash][i] == str) {
+        table_of_values_[hash][i] += value;
+        return table_of_values_[hash][i];
+      }
+    }
+
+    table_of_values_[hash].push_back(value);
+    table_of_strings_[hash].push_back(str);
+
+    return value;
+  }
+
  private:
   const long long M = 10000379;
   const long long a = 179;
   std::vector<std::vector<int>> table_of_values_;
-  std::vector<std::vector<std::string>> tables_of_str_;
+  std::vector<std::vector<std::string>> table_of_strings_;
 
   long long Hash(const std::string& str) {
     long long result = str[0];
@@ -17,25 +36,6 @@ class HashTable {
     }
 
     return result;
-  }
-
- public:
-  HashTable() : table_of_values_(M), tables_of_str_(M) {}
-
-  long long AddAndReturnValue(const std::string& str, long long value) {
-    long long hash = Hash(str);
-
-    for (int i = 0; i < tables_of_str_[hash].size(); ++i) {
-      if (tables_of_str_[hash][i] == str) {
-        table_of_values_[hash][i] += value;
-        return table_of_values_[hash][i];
-      }
-    }
-
-    table_of_values_[hash].push_back(value);
-    tables_of_str_[hash].push_back(str);
-
-    return value;
   }
 };
 
