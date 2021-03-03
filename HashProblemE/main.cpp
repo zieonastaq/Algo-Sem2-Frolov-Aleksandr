16f210bac8c0aa4a086b1b6a41491b9bd3a457c9
zieonasta <zieonasta@ya.ru>
2021-03-03 13:50:36 +0300

    Hash problem E some changes

--- HashProblemE/main.cpp
+++ HashProblemE/main.cpp
@@ -1,33 +1,18 @@
 #include <fstream>
 #include <list>
 #include <string>
+#include <utility>
 #include <vector>
 
 class HashTable {
- private:
-  static const size_t TABLE_SIZE_ = 999'979;
-  static const size_t PRIME_CONST_ = 31;
-
-  std::vector<std::list<std::pair<std::string, std::string>>> table_;
-
-  static size_t hash_function_(const std::string& str) {
-    auto result = static_cast<size_t>(str[0]);
-
-    for (int i = 1; i < str.size(); ++i) {
-      result = ((result * PRIME_CONST_) % TABLE_SIZE_ + str[i]) % TABLE_SIZE_;
-    }
-
-    return result;
-  }
-
  public:
   HashTable() : table_(TABLE_SIZE_) {}
 
-  void Put(const std::string& x, std::string y) {
+  void Put(const std::string& x, const std::string& y) {
     size_t hash_of_x = hash_function_(x);
     for (auto& i : table_[hash_of_x]) {
-      if (i.first == x) {
-        i.second = y;
+      if (i.key_ == x) {
+        i.value_ = y;
         return;
       }
     }
@@ -38,7 +23,7 @@ class HashTable {
   void Delete(const std::string& x) {
     size_t hash_of_x = hash_function_(x);
     for (auto i = table_[hash_of_x].begin(); i != table_[hash_of_x].end(); ++i) {
-      if (i->first == x) {
+      if (i->key_ == x) {
         table_[hash_of_x].erase(i);
         return;
       }
@@ -48,12 +33,37 @@ class HashTable {
   std::string Get(const std::string& x) {
     size_t hash_of_x = hash_function_(x);
     for (auto& i : table_[hash_of_x]) {
-      if (i.first == x) {
-        return i.second;
+      if (i.key_ == x) {
+        return i.value_;
       }
     }
 
     return "none";
+  }
+
+ private:
+  template <typename KeyType, typename ValueType>
+  class KeyAndValue {
+   public:
+    KeyType key_;
+    ValueType value_;
+
+    KeyAndValue(KeyType key, ValueType value) : key_(std::move(key)), value_(std::move(value)) {}
+  };
+
+  static const size_t TABLE_SIZE_ = 999'979;
+  static const size_t PRIME_CONST_ = 31;
+
+  std::vector<std::list<KeyAndValue<std::string, std::string>>> table_;
+
+  static size_t hash_function_(const std::string& str) {
+    auto result = static_cast<size_t>(str[0]);
+
+    for (int i = 1; i < str.size(); ++i) {
+      result = ((result * PRIME_CONST_) % TABLE_SIZE_ + str[i]) % TABLE_SIZE_;
+    }
+
+    return result;
   }
 };
 
