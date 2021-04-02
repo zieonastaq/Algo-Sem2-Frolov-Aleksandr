b314c700bc4f428fee0d267d1d64c1878316f524
zieonasta <zieonasta@ya.ru>
2021-04-02 10:45:19 +0300

    dp 2 problem K

---
+++ DP2ProblemK/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+bool check(long long i, long long j, long long m){
+  long long checker1 = -1, checker2 = -1;
+
+  for (size_t k = 0; k < m; ++k) {
+    if (checker1 == checker2 &&
+        checker1 == i % 2 && checker1 == j % 2) {
+      return false;
+    }
+
+    checker1 = i % 2;
+    checker2 = j % 2;
+    i /= 2;
+    j /= 2;
+  }
+
+  return true;
+}
+
+int main() {
+  long long n, m;
+  std::cin >> n >> m;
+
+  if (m > n) {
+    std::swap(m, n);
+  }
+
+  long long power2m = 1 << m;
+
+  std::vector<std::vector<long long>> dp(n, std::vector<long long>(power2m));
+
+  for (auto& i : dp[0]) {
+    i = 1;
+  }
+
+  /*std::vector<std::vector<bool>> d(power2n, std::vector<bool>(power2n));
+
+  for (long long i = 0; i < power2n; ++i) {
+    for (long long j = 0; j < power2n; ++j) {
+      d[i][j] = check(i, j);
+    }
+  }*/
+
+  for (long long k = 1; k < n; ++k) {
+    for (long long i = 0; i < power2m; ++i) {
+      for (long long j = 0; j < power2m; ++j) {
+        //dp[k][i] = dp[k - 1][j] * d[j][i] + dp[k][i];
+        if (check(i, j, m)) {
+          dp[k][i] += dp[k - 1][j];
+        }
+      }
+    }
+  }
+
+  long long ans = 0;
+
+  for (auto i : dp.back()) {
+    ans += i;
+  }
+
+  std::cout << ans;
+
+  return 0;
+}
